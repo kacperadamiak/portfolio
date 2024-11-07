f081fc129476b63fa63339177811a9e591f759c2
Kacper Adamiak <kacper01adamiak@gmail.com>
2024-11-07 11:01:53 +0100

    new filter version, small changes in showing title.

--- title.js
+++ title.js
@@ -3,8 +3,11 @@ function updateHighlightText() {
 
     if (window.innerWidth <= 800) {
         highlightElement.textContent = "AK";
+        if (window.innerWidth <= 500) {
+            highlightElement.textContent = "";
+        }
     } else {
-        highlightElement.textContent = "Kacper Adamiak";
+        highlightElement.textContent = "Kacper Adamiak"; 
     }
 }
 window.addEventListener('load', updateHighlightText);
